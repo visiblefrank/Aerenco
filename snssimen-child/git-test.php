bc53c9b3c4ea066c42427234c78346b6128e3701
Frank Keane <frankjkeane@me.com>
2016-06-30 16:43:27 +0100

    added test html page

---
+++ snssimen-child/git-test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><<**SelectionInsertionPlaceholder**>></title>
+	</head>
+	<body>
+		<p>test 1</p>
+	</body>
+</html>
